646c7853c8674fa14a057a77d2ca542567ea13c7
mabdullah2005 <mabdullah2005967@gmail.com>
2025-11-10 23:22:01 +0000

    Developed Card Class Logic

---
+++ game/Card.py
@@ -0,0 +1,33 @@
+# Name: Card
+# Description:
+#   Holds all logic of a singular card
+
+# Attributes:
+#   suit: Suit of the card (eg/ Hearts, Diamonds, Clubs, Spades)
+#   rank: Rank of the card (eg/ 2 - 10, Jack, Queen, King, Ace)
+#   value: Value of Corresponding rank
+
+class Card:
+    _suit: str
+    _rank: str 
+    _value: int
+
+    def __init__(self, rank:str, suit:str, value: int):
+        self._suit = suit
+        self._rank = rank
+        self._value = value
+
+    @property
+    def suit(self):
+        return self._suit 
+
+    @property
+    def rank(self):
+        return self._rank 
+    
+    @property
+    def value(self):
+        return self._value
+
+    def __str__(self):
+        return "{} of {}".format(self.rank, self.suit)   
